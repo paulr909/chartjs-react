9ffd227ce322fec79b75fce3939ffc531fde4b6c
Paul Rogers <paulrogers909@protonmail.com>
2020-06-03 12:14:47 +0100

    comment out import

--- src/components/dashboards/Dashboard.js
+++ src/components/dashboards/Dashboard.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import BubbleChart from "../charts/BubbleChart";
-import DoughnutChart from "../charts/DoughnutChart";
+// import DoughnutChart from "../charts/DoughnutChart";
 import PieChart from "../charts/PieChart";
 import chartIcon from "../../assets/chart-icon.svg";
 import classes from "./Dashboard.module.css";
