e48e252e3f7340eba28edc912864cfb15fcb6400
Paul Rogers <paulrogers909@protonmail.com>
2020-06-03 14:14:51 +0100

    fix router warning

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Dashboard from "./components/dashboards/Dashboard";
 import SalesDashboard from "./components/dashboards/SalesDashboard";
 
 const App = () => {
   return (
     <div>
-      <Switch>
-        <Route exact path="/" component={Dashboard}>
-          <Dashboard />
-        </Route>
-        <Route path="/sales-dashboard" component={SalesDashboard}>
-          <SalesDashboard />
-        </Route>
-      </Switch>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Dashboard />
+          </Route>
+          <Route path="/sales-dashboard">
+            <SalesDashboard />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 };
