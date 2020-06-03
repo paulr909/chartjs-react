cf50867edfb792322d0495498a5c6ecf5d8ccd58
Paul Rogers <paulrogers909@protonmail.com>
2020-06-03 12:13:46 +0100

    edit PieChart.js

--- src/components/dashboards/Dashboard.js
+++ src/components/dashboards/Dashboard.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import BubbleChart from "../charts/BubbleChart";
 import DoughnutChart from "../charts/DoughnutChart";
-// import PieChart from "../charts/PieChart";
+import PieChart from "../charts/PieChart";
 import chartIcon from "../../assets/chart-icon.svg";
 import classes from "./Dashboard.module.css";
 
@@ -15,8 +15,8 @@ const Dashboard = () => {
         <Link to="/sales-dashboard">Sales Dashboard</Link>
       </header>
       <BubbleChart />
-      <DoughnutChart />
-      {/*<PieChart />*/}
+      {/*<DoughnutChart />*/}
+      <PieChart />
     </div>
   );
 };
